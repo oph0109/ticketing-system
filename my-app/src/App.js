3e8c7462feeeedafa8a9dea217c8fcd6d2ca3848
liammathes <liam.mathes19@gmail.com>
2024-02-13 20:09:26 -0600

    created app with raygn logo

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './images/raygn.png';
 import './App.css';
 
 function App() {
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to your Ticketing System.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="http://pbs.twimg.com/media/BG48ENgCEAAIDl9.jpg"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Click here to create an account
         </a>
       </header>
     </div>
