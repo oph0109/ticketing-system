df836223c0d4b2d4b8dfa8a74b2f46a11a5feeb9
Matthew Burseth <mattburseth1@gmail.com>
2024-02-18 22:02:11 -0600

    Signup and Login functioning
    Sign up and login are working for the most part, im getting a weird JSON error on the response when invalid login creds are supplied

--- api/users.js
+++ api/users.js
@@ -3,7 +3,7 @@
 const BASE_API_CALL = "http://localhost:8080";
 
 //returns array of json objects representing users - NOT USED
-export async function getUsers() {
+export default async function getUsers() {
   const users = await fetch(BASE_API_CALL + "/api/users", {
     method: "GET",
     headers: {
@@ -16,7 +16,7 @@ export async function getUsers() {
 
 //takes the form data from the submitted HTML form and creates an api call using fetchAPI to Spring server
 //formData is a FormData object - must be this way because submitting a form straight from HTMl does not allow for response handling/redirecting
-export default async function signupSingleUserWithFormData(formData) {
+export async function signupUserWithFormData(formData) {
   const signupResult = await fetch(BASE_API_CALL + "/api/users", {
     method: "POST",
     headers: {
@@ -30,7 +30,22 @@ export default async function signupSingleUserWithFormData(formData) {
       return error;
     });
 
-  console.log(signupResult);
-
   return signupResult;
+}
+
+export async function loginUserWithFormData(formData) {
+  const loginResult = await fetch(BASE_API_CALL + "/api/auth/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(Object.fromEntries(formData)),
+  })
+    .then((response) => response.json())
+    .catch((error) => {
+      console.log("Error: " + error);
+      return error;
+    });
+
+  return loginResult;
 }
--- script.js
+++ script.js
@@ -2,17 +2,53 @@
 const REACT_SITE = "http://localhost:3000";
 
 //import functions from users.js file as 'users'
-import signupSingleUserWithFormData from "./api/users.js";
+import { signupUserWithFormData, loginUserWithFormData } from "./api/users.js";
 
-//attaches an eventlistener to the signup button
-// document.getElementById("signup-form").addEventListener("submit", (e) => {
-//   e.preventDefault();
+//attaches an eventlistener to the login button
+//because the login form has only two fields and the endpoint expects three (the username is missing),
+//we have to falsely add a username key value pair - the username is just set to the email
+document.getElementById("login-signup-form").addEventListener("submit", (e) => {
+  e.preventDefault();
 
-//   signupUser(new FormData(e.target));
-// });
+  var formData = new FormData(e.target);
+  formData.append("username", formData.get("email"));
+
+  loginUser(formData);
+});
+
+//attaches an eventlistener to signup button
+document.getElementById("signup-form").addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  signupUser(new FormData(e.target));
+});
+
+//makes an api call to the auth/login endpoint - not sure what the fuck this returns WIP
+async function loginUser(formData) {
+  const loginResult = await loginUserWithFormData(formData);
+  var loginMessage = document.getElementById("login-error");
+
+  console.log("result: " + loginResult);
+
+  if (loginResult.status === 400) {
+    loginMessage.textContent = "Invalid credentials faggot.";
+    loginMessage.style.color = "red";
+
+    return;
+  } else {
+    loginMessage.textContent = "Success! Redirecting...";
+    loginMessage.style.color = "green";
+
+    window.setTimeout(function () {
+      // Move to a new location or you can do something else
+      //Also delays 3 seconds to make it look like its loading nicely
+      window.location.href = "http://localhost:3000/?uuid=" + loginResult.uuid;
+    }, 10000000);
+  }
+}
 
 async function signupUser(formData) {
-  const signupResult = await signupSingleUserWithFormData(formData);
+  const signupResult = await signupUserWithFormData(formData);
   var signupMessage = document.getElementById("signup-error");
 
   console.log(signupResult);
@@ -28,8 +64,6 @@ async function signupUser(formData) {
     signupMessage.textContent = "Success! Redirecting...";
     signupMessage.style.color = "green";
 
-    console.log(signupResult.uuid);
-
     window.setTimeout(function () {
       // Move to a new location or you can do something else
       //Also delays 3 seconds to make it look like its loading nicely
@@ -39,7 +73,7 @@ async function signupUser(formData) {
 }
 
 // Get the modal
-const modal = document.getElementById('signup-modal');
+const modal = document.getElementById("signup-modal");
 
 // Get the button that opens the modal
 const btn = document.getElementById("signup-btn");
@@ -47,21 +81,19 @@ const btn = document.getElementById("signup-btn");
 // Get the <span> element that closes the modal
 const span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
+// When the user clicks the button, open the modal
+btn.onclick = function () {
   modal.style.display = "block";
-}
+};
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+span.onclick = function () {
   modal.style.display = "none";
-}
+};
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-}
-
-
+};
